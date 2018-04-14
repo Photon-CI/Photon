e71c1679f9c79d7f59e91d5ccb6bc22f771ca071
JoshuaMiller <admin@joshuamiller.info>
2018-04-14 14:54:03 -0400

    build fix

--- bin/msbuild_where.cmd
+++ bin/msbuild_where.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-for /f "usebackq tokens=*" %%i in (`vswhere -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
+for /f "usebackq tokens=*" %%i in (`%~dp0\vswhere.exe -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
   set InstallDir=%%i
 )
 
