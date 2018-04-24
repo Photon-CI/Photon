587635f69cb26f1b326beee3ddcece4ca2b616c6
JoshuaMiller <admin@joshuamiller.info>
2018-04-24 00:26:00 -0400

    iis package; msbuild fix

--- bin/msbuild.cmd
+++ bin/msbuild.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-for /f "usebackq tokens=*" %%i in (`%~dp0vswhere -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
+for /f "usebackq tokens=*" %%i in (`%~dp0vswhere -latest -requires Microsoft.Component.MSBuild -property installationPath`) do (
   set InstallDir=%%i
 )
 
