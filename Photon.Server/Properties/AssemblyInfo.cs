16f51db46de1469bacb02b831c230f922caa0b53
JoshuaMiller <admin@joshuamiller.info>
2018-04-25 22:29:34 -0400

    server update pretest

--- Photon.Server/Properties/AssemblyInfo.cs
+++ Photon.Server/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.0.29")]
-[assembly: AssemblyFileVersion("0.0.29")]
+[assembly: AssemblyVersion("0.0.30")]
+[assembly: AssemblyFileVersion("0.0.30")]
 
 [assembly: AssemblyTitle("Photon Server")]
 [assembly: AssemblyDescription("Primary service for Photon.")]
