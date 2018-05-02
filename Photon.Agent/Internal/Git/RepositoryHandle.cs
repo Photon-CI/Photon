2844eac3651187fe88f763ac4194e5c6bd3871e3
JoshuaMiller <admin@joshuamiller.info>
2018-05-02 15:22:00 -0400

    revised checkout logic

--- Photon.Agent/Internal/Git/RepositoryHandle.cs
+++ Photon.Agent/Internal/Git/RepositoryHandle.cs
@@ -26,6 +26,11 @@ namespace Photon.Agent.Internal.Git
 
         public void Checkout(SessionOutput output, string refspec = "master")
         {
+            var checkoutOptions = new CheckoutOptions {
+                CheckoutModifiers = CheckoutModifiers.Force,
+            };
+
+            // Clone repository if it does not exist
             if (!Repository.IsValid(Source.RepositoryPath)) {
                 output.WriteLine("Cloning Repository...", ConsoleColor.DarkCyan);
 
@@ -36,6 +41,7 @@ namespace Photon.Agent.Internal.Git
             }
 
             using (var repo = new Repository(Source.RepositoryPath)) {
+                // Fetch all updated refspecs and tags
                 output.WriteLine("Fetching updated refs...", ConsoleColor.DarkCyan);
 
                 var fetchSpec = new[] {"+refs/heads/*:refs/remotes/origin/*"};
@@ -47,37 +53,55 @@ namespace Photon.Agent.Internal.Git
 
                 LibGit2Sharp.Commands.Fetch(repo, "origin", fetchSpec, fetchOptions, null);
 
-                var localBranch = repo.Branches[$"refs/heads/origin/{refspec}"];
+                // Find local and remote branches
+                var remoteBranchName = $"refs/remotes/origin/{refspec}";
+                var remoteBranch = repo.Branches[remoteBranchName];
+
+                var localBranchName = $"refs/heads/origin/{refspec}";
+                var localBranch = repo.Branches[localBranchName];
+
+                if (remoteBranch == null) {
+                    output.Write("Git Refspec ", ConsoleColor.DarkYellow)
+                        .Write(refspec, ConsoleColor.Yellow)
+                        .WriteLine(" was not found!", ConsoleColor.DarkYellow);
+
+                    throw new ApplicationException($"Git Refspec '{refspec}' was not found!");
+                }
 
                 if (localBranch != null) {
                     output.WriteLine($"Found local branch '{localBranch.FriendlyName}'...", ConsoleColor.DarkCyan);
 
-                    if (!localBranch.IsCurrentRepositoryHead) {
-                        output.WriteLine($"Checkout local branch '{localBranch.FriendlyName}'...", ConsoleColor.DarkCyan);
-
-                        var checkoutOptions = new CheckoutOptions {
-                            CheckoutModifiers = CheckoutModifiers.Force,
-                        };
-
-                        LibGit2Sharp.Commands.Checkout(repo, localBranch, checkoutOptions);
-                    }
-
-                    if (!localBranch.IsTracking) {
-                        output.WriteLine("Updating branch remote tracking ref...", ConsoleColor.DarkCyan);
-
-                        var remoteBranch = repo.Branches[$"refs/remotes/origin/{refspec}"];
-
-                        if (remoteBranch == null) {
-                            output.Write("Git Refspec ", ConsoleColor.DarkYellow)
-                                .Write(refspec, ConsoleColor.Yellow)
-                                .WriteLine(" was not found!", ConsoleColor.DarkYellow);
-                    
-                            throw new ApplicationException($"Git Refspec '{refspec}' was not found!");
-                        }
+                    // Update tracking branch if not remote branch
+                    if (!localBranch.IsTracking || localBranch.TrackedBranch != remoteBranch) {
+                        output.WriteLine("Updating local branch tracking reference...", ConsoleColor.DarkCyan);
 
                         repo.Branches.Update(localBranch, b => b.TrackedBranch = remoteBranch.CanonicalName);
                     }
 
+                    // Checkout local branch if not current
+                    if (!localBranch.IsCurrentRepositoryHead) {
+                        output.WriteLine($"Checkout local branch '{localBranch.FriendlyName}'...", ConsoleColor.DarkCyan);
+
+                        LibGit2Sharp.Commands.Checkout(repo, localBranch, checkoutOptions);
+                    }
+
+                    // Revert to common ancestor commit if diverged
+                    var status = localBranch.TrackingDetails;
+                    var aheadCount = status.AheadBy ?? 0;
+
+                    if (aheadCount > 0) {
+                        output.WriteLine($"Local branch '{localBranch.FriendlyName}' has diverged from the remote tracking branch!", ConsoleColor.DarkYellow);
+
+                        var common = status.CommonAncestor;
+
+                        if (common != null) {
+                            output.WriteLine($"Reverting local branch to commit '{common.Sha}'!", ConsoleColor.DarkCyan);
+
+                            repo.Reset(ResetMode.Hard, common, checkoutOptions);
+                        }
+                    }
+
+                    // Pull latest changes from remote
                     output.WriteLine("Pull changes from remote...", ConsoleColor.DarkCyan);
 
                     var sign = new Signature("photon", "photon@localhost.com", DateTimeOffset.Now);
@@ -89,27 +113,12 @@ namespace Photon.Agent.Internal.Git
                     LibGit2Sharp.Commands.Pull(repo, sign, pullOptions);
                 }
                 else {
-                    var remoteBranch = repo.Branches[$"refs/remotes/origin/{refspec}"];
-
-                    if (remoteBranch == null) {
-                        output.Write("Git Refspec ", ConsoleColor.DarkYellow)
-                            .Write(refspec, ConsoleColor.Yellow)
-                            .WriteLine(" was not found!", ConsoleColor.DarkYellow);
-                    
-                        throw new ApplicationException($"Git Refspec '{refspec}' was not found!");
-                    }
-
-                    output.WriteLine($"Found remote branch '{remoteBranch.FriendlyName}'...", ConsoleColor.DarkCyan);
-                    output.WriteLine("Creating local tracking branch...", ConsoleColor.DarkCyan);
-
+                    // Create local branch tracking remote
+                    output.WriteLine($"No local branch found. Creating local tracking branch '{remoteBranch.FriendlyName}'...", ConsoleColor.DarkCyan);
                     localBranch = repo.CreateBranch(remoteBranch.FriendlyName, remoteBranch.Tip);
                     repo.Branches.Update(localBranch, b => b.TrackedBranch = remoteBranch.CanonicalName);
 
                     output.WriteLine($"Checkout local tracking branch '{localBranch.FriendlyName}'...", ConsoleColor.DarkCyan);
-
-                    var checkoutOptions = new CheckoutOptions {
-                        CheckoutModifiers = CheckoutModifiers.Force,
-                    };
 
                     LibGit2Sharp.Commands.Checkout(repo, localBranch, checkoutOptions);
                 }
