d028a1aaa64f3fe7e73d70eee6e63f1cc298f5f0
JoshuaMiller <admin@joshuamiller.info>
2018-05-15 14:48:24 -0400

    publish

--- Photon.Framework/Properties/AssemblyInfo.cs
+++ Photon.Framework/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.0.20")]
-[assembly: AssemblyFileVersion("0.0.20")]
+[assembly: AssemblyVersion("0.0.21")]
+[assembly: AssemblyFileVersion("0.0.21")]
 
 [assembly: AssemblyTitle("Photon Framework")]
 [assembly: AssemblyDescription("Framework for Photon projects and plugins.")]
