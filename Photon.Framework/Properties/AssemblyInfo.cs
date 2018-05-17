ce48a665dba9c8ef9a2a73fdc99bc40e73453ec7
JoshuaMiller <admin@joshuamiller.info>
2018-05-17 00:45:44 -0400

    fix server refs

--- Photon.Framework/Properties/AssemblyInfo.cs
+++ Photon.Framework/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.0.25")]
-[assembly: AssemblyFileVersion("0.0.25")]
+[assembly: AssemblyVersion("0.0.26")]
+[assembly: AssemblyFileVersion("0.0.26")]
 
 [assembly: AssemblyTitle("Photon Framework")]
 [assembly: AssemblyDescription("Framework for Photon projects and plugins.")]
