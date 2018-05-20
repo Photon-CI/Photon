ff07de42a3331888bc0737cbf6e6d028181c5a80
JoshuaMiller <admin@joshuamiller.info>
2018-05-20 16:22:52 -0400

    linux nuget restore

--- bin/PreBuild.sh
+++ bin/PreBuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-nuget.exe restore "./bin/Photon.sln"
+mono "./bin/NuGet.exe" restore "./Photon.sln"
 
 msbuild.exe "./Photon.Publishing/Photon.Publishing.csproj" /t:Rebuild /p:Configuration="Debug" /p:Platform="Any CPU" /p:OutputPath="bin\Debug" /v:m
