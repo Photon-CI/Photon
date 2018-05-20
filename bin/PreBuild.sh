605e23e44c638f3b1b0acd59c220f2d9125ab386
JoshuaMiller <admin@joshuamiller.info>
2018-05-20 17:05:07 -0400

    linux msbuild

--- bin/PreBuild.sh
+++ bin/PreBuild.sh
@@ -2,4 +2,4 @@
 
 mono "./bin/NuGet.exe" restore "./Photon.sln"
 
-mono "msbuild.exe" "./Photon.Publishing/Photon.Publishing.csproj" /t:Rebuild /p:Configuration="Debug" /p:Platform="Any CPU" /p:OutputPath="bin\Debug" /v:m
+msbuild "./Photon.Publishing/Photon.Publishing.csproj" /t:Rebuild /p:Configuration="Debug" /p:Platform="Any CPU" /p:OutputPath="bin\Debug" /v:m
