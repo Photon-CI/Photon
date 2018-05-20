30b89f5b54ea549f4ea614017ddabd4397fa5d16
JoshuaMiller <admin@joshuamiller.info>
2018-05-20 19:22:21 -0400

    cancel fix

--- Photon.Agent/Properties/AssemblyInfo.cs
+++ Photon.Agent/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.0.96")]
-[assembly: AssemblyFileVersion("0.0.96")]
+[assembly: AssemblyVersion("0.0.97")]
+[assembly: AssemblyFileVersion("0.0.97")]
 
 [assembly: AssemblyTitle("Photon Agent")]
 [assembly: AssemblyDescription("Agent service for Photon.")]
