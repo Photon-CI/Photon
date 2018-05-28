7c764e39acc63f40378125865f5aa650b8f33899
JoshuaMiller <admin@joshuamiller.info>
2018-05-28 02:05:51 -0400

    release

--- Plugins/Photon.Nuget/Properties/AssemblyInfo.cs
+++ Plugins/Photon.Nuget/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.0.2")]
-[assembly: AssemblyFileVersion("0.0.2")]
+[assembly: AssemblyVersion("0.0.3")]
+[assembly: AssemblyFileVersion("0.0.3")]
 
 [assembly: AssemblyTitle("Photon.Nuget")]
 [assembly: AssemblyDescription("NuGet plugin for Photon.")]
