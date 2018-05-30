0425da37fb98cd2f23a13f60f281b5f833d8112a
JoshuaMiller <admin@joshuamiller.info>
2018-05-30 13:17:03 -0400

    release

--- Photon.Agent/Properties/AssemblyInfo.cs
+++ Photon.Agent/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.0.113")]
-[assembly: AssemblyFileVersion("0.0.113")]
+[assembly: AssemblyVersion("0.0.114")]
+[assembly: AssemblyFileVersion("0.0.114")]
 
 [assembly: AssemblyTitle("Photon Agent")]
 [assembly: AssemblyDescription("Agent service for Photon.")]
