e1c8fdba7cb4cbfd1d6ffc7f91de600e382d7292
JoshuaMiller <admin@joshuamiller.info>
2018-06-01 17:26:10 -0400

    json encoding fix

--- Photon.Server/Properties/AssemblyInfo.cs
+++ Photon.Server/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.0.119")]
-[assembly: AssemblyFileVersion("0.0.119")]
+[assembly: AssemblyVersion("0.0.120")]
+[assembly: AssemblyFileVersion("0.0.120")]
 
 [assembly: AssemblyTitle("Photon Server")]
 [assembly: AssemblyDescription("Primary service for Photon.")]
