d5440491ab93eea3ed1bb4d2c006937f45156ffc
JoshuaMiller <admin@joshuamiller.info>
2018-05-31 23:06:47 -0400

    agent ui fix

--- Photon.Agent/Properties/AssemblyInfo.cs
+++ Photon.Agent/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.0.116")]
-[assembly: AssemblyFileVersion("0.0.116")]
+[assembly: AssemblyVersion("0.0.118")]
+[assembly: AssemblyFileVersion("0.0.118")]
 
 [assembly: AssemblyTitle("Photon Agent")]
 [assembly: AssemblyDescription("Agent service for Photon.")]
