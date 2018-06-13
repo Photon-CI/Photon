657a9c713be2cb627f987ac00c895dad81e38c06
JoshuaMiller <admin@joshuamiller.info>
2018-06-13 18:14:44 -0400

    serializable domainblockwriter

---
+++ Photon.Framework/Domain/DomainBlockWriter.cs
@@ -0,0 +1,54 @@
+﻿using Photon.Framework.Server;
+using System;
+
+namespace Photon.Framework.Domain
+{
+    [Serializable]
+    public class DomainBlockWriter : IDisposable
+    {
+        private readonly DomainOutput domainOutput;
+        private readonly ScriptOutput output;
+
+
+        public DomainBlockWriter(DomainOutput output)
+        {
+            this.domainOutput = output;
+
+            this.output = new ScriptOutput();
+        }
+
+        public void Dispose()
+        {
+            output?.Dispose();
+        }
+
+        public DomainBlockWriter Write(string text, ConsoleColor color)
+        {
+            output.Write(text, color);
+            return this;
+        }
+
+        public DomainBlockWriter Write(object value, ConsoleColor color)
+        {
+            output.Write(value, color);
+            return this;
+        }
+
+        public DomainBlockWriter WriteLine(string text, ConsoleColor color)
+        {
+            output.WriteLine(text, color);
+            return this;
+        }
+
+        public DomainBlockWriter WriteLine(object value, ConsoleColor color)
+        {
+            output.WriteLine(value, color);
+            return this;
+        }
+
+        public void Post()
+        {
+            domainOutput.WriteRaw(output.GetString());
+        }
+    }
+}
--- Photon.Framework/Domain/DomainOutput.cs
+++ Photon.Framework/Domain/DomainOutput.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Photon.Framework.Server;
 
 namespace Photon.Framework.Domain
 {
@@ -47,54 +46,6 @@ namespace Photon.Framework.Domain
         public DomainBlockWriter WriteBlock()
         {
             return new DomainBlockWriter(this);
-        }
-    }
-
-    public class DomainBlockWriter : IDisposable
-    {
-        private readonly DomainOutput domainOutput;
-        private readonly ScriptOutput output;
-
-
-        public DomainBlockWriter(DomainOutput output)
-        {
-            this.domainOutput = output;
-
-            this.output = new ScriptOutput();
-        }
-
-        public void Dispose()
-        {
-            output?.Dispose();
-        }
-
-        public DomainBlockWriter Write(string text, ConsoleColor color)
-        {
-            output.Write(text, color);
-            return this;
-        }
-
-        public DomainBlockWriter Write(object value, ConsoleColor color)
-        {
-            output.Write(value, color);
-            return this;
-        }
-
-        public DomainBlockWriter WriteLine(string text, ConsoleColor color)
-        {
-            output.WriteLine(text, color);
-            return this;
-        }
-
-        public DomainBlockWriter WriteLine(object value, ConsoleColor color)
-        {
-            output.WriteLine(value, color);
-            return this;
-        }
-
-        public void Post()
-        {
-            domainOutput.WriteRaw(output.GetString());
         }
     }
 }
