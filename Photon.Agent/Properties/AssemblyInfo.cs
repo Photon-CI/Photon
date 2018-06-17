9aebdc462febc982979329a2565efbc269543085
JoshuaMiller <admin@joshuamiller.info>
2018-06-17 16:18:35 -0400

    tcp flush fixed; release

--- Photon.Agent/Properties/AssemblyInfo.cs
+++ Photon.Agent/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.0.142")]
-[assembly: AssemblyFileVersion("0.0.142")]
+[assembly: AssemblyVersion("0.0.143")]
+[assembly: AssemblyFileVersion("0.0.143")]
 
 [assembly: AssemblyTitle("Photon Agent")]
 [assembly: AssemblyDescription("Agent service for Photon.")]
