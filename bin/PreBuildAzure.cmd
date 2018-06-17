9ec4bd856ab35d1bbb8b8d4fa758f467077adf2d
JoshuaMiller <admin@joshuamiller.info>
2018-06-17 17:46:19 -0400

    repair ci

--- bin/PreBuildAzure.cmd
+++ bin/PreBuildAzure.cmd
@@ -2,5 +2,5 @@
 if not %errorlevel% == 0 exit %errorlevel%
 
 set msbuild_exe="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe"
-"%msbuild_exe%" "%~dp0..\Photon.Publishing\Photon.Publishing.csproj" /t:Rebuild /p:Configuration="Debug" /p:Platform="Any CPU" /p:OutputPath="bin\Debug" /v:m
+%msbuild_exe% "%~dp0..\Photon.Publishing\Photon.Publishing.csproj" /t:Rebuild /p:Configuration="Debug" /p:Platform="Any CPU" /p:OutputPath="bin\Debug" /v:m
 if not %errorlevel% == 0 exit %errorlevel%
