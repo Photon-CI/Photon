1c5fa1349cf176f6e37e9ed182699520086749ed
JoshuaMiller <admin@joshuamiller.info>
2018-07-28 01:16:47 -0400

    release

--- Photon.Server/Properties/AssemblyInfo.cs
+++ Photon.Server/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.0.150")]
-[assembly: AssemblyFileVersion("0.0.150")]
+[assembly: AssemblyVersion("0.0.151")]
+[assembly: AssemblyFileVersion("0.0.151")]
 
 [assembly: AssemblyTitle("Photon Server")]
 [assembly: AssemblyDescription("Primary service for Photon.")]
