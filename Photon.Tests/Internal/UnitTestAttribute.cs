bf973fd47beab4c2b5ef746be25ae64e02ae55cf
JoshuaMiller <admin@joshuamiller.info>
2018-09-02 18:16:02 -0400

    test cat fix

--- Photon.Tests/Internal/UnitTestAttribute.cs
+++ Photon.Tests/Internal/UnitTestAttribute.cs
@@ -1,7 +1,9 @@
 ﻿using NUnit.Framework;
+using System;
 
 namespace Photon.Tests.Internal
 {
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
     internal class UnitTestAttribute : CategoryAttribute
     {
         public UnitTestAttribute() : base("unit") {}
