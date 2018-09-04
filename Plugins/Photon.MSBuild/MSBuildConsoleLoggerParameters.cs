8ffaa6ee7862bb8207d13cb36dee167a617aea1a
JoshuaMiller <admin@joshuamiller.info>
2018-09-03 20:18:47 -0400

    msbuild verbosity flag fix

--- Plugins/Photon.MSBuild/MSBuildConsoleLoggerParameters.cs
+++ Plugins/Photon.MSBuild/MSBuildConsoleLoggerParameters.cs
@@ -5,75 +5,75 @@ namespace Photon.MSBuild
     [Flags]
     public enum MSBuildConsoleLoggerParameters
     {
-        Default,
+        Default = 0,
 
         /// <summary>
         /// Show the time that's spent in tasks, targets, and projects.
         /// </summary>
-        PerformanceSummary,
+        PerformanceSummary = 1,
 
         /// <summary>
         /// Show the error and warning summary at the end.
         /// </summary>
-        Summary,
+        Summary = 2,
 
         /// <summary>
         /// Don't show the error and warning summary at the end.
         /// </summary>
-        NoSummary,
+        NoSummary = 4,
 
         /// <summary>
         /// Show only errors.
         /// </summary>
-        ErrorsOnly,
+        ErrorsOnly = 8,
 
         /// <summary>
         /// Show only warnings.
         /// </summary>
-        WarningsOnly,
+        WarningsOnly = 16,
 
         /// <summary>
         /// Don't show the list of items and properties that would appear at the
         /// start of each project build if the verbosity level is set to diagnostic.
         /// </summary>
-        NoItemAndPropertyList,
+        NoItemAndPropertyList = 32,
 
         /// <summary>
         /// Show TaskCommandLineEvent messages.
         /// </summary>
-        ShowCommandLine,
+        ShowCommandLine = 64,
 
         /// <summary>
         /// Show the timestamp as a prefix to any message.
         /// </summary>
-        ShowTimestamp,
+        ShowTimestamp = 128,
 
         /// <summary>
         /// Show the event ID for each started event,
         /// finished event, and message.
         /// </summary>
-        ShowEventId,
+        ShowEventId = 256,
 
         /// <summary>
         /// Don't align the text to the size of the console buffer.
         /// </summary>
-        ForceNoAlign,
+        ForceNoAlign = 512,
 
         /// <summary>
         /// Use the default console colors for all logging messages.
         /// </summary>
-        DisableConsoleColor,
+        DisableConsoleColor = 1024,
 
         /// <summary>
         /// Disable the multiprocessor logging style of output when
         /// running in non-multiprocessor mode.
         /// </summary>
-        DisableMPLogging,
+        DisableMPLogging = 2048,
 
         /// <summary>
         /// Enable the multiprocessor logging style even when running in
         /// non-multiprocessor mode. This logging style is on by default.
         /// </summary>
-        EnableMPLogging,
+        EnableMPLogging = 4096,
     }
 }
